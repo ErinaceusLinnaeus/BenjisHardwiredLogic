ad7abb478c51fb7906aeb8260a35813693c8269e
ErinaceusLinnaeus <mail@benji.is>
2024-09-15 17:42:33 +0000

    removing a space

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BenjisHardwiredLogic")]
-[assembly: AssemblyCopyright("Copyright ©  2021-2024")]
+[assembly: AssemblyCopyright("Copyright © 2021-2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
