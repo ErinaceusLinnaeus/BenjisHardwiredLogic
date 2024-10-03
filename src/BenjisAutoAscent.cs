f39aab2d9139a9cd0b80d60cb472df8780a5a55d
ErinaceusLinnaeus <mail@benji.is>
2024-10-03 19:48:06 +0000

    What to calculate...
    ...to get the best downrange estimate.

--- src/BenjisAutoAscent.cs
+++ src/BenjisAutoAscent.cs
@@ -51,9 +51,9 @@ namespace BenjisHardwiredLogic
         private float totalGuidedFlight = 0;
 
         //Specify the angle you wanna end up at
-        [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = true, guiName = "Ballistic Angle [°]", guiFormat = "F1", groupName = PAWAscentGroupName, groupDisplayName = PAWAscentGroupName),
-            UI_FloatEdit(scene = UI_Scene.All, minValue = 40f, maxValue = 50.0f, incrementLarge = 5f, incrementSmall = 1f, incrementSlide = 0.1f, sigFigs = 1)]
-        private float desiredBallisitcAngle = 45;
+        [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = true, guiName = "DeltaV [m/s]", guiFormat = "F1", groupName = PAWAscentGroupName, groupDisplayName = PAWAscentGroupName),
+            UI_FloatEdit(scene = UI_Scene.All, minValue = 0f, maxValue = 8000.0f, incrementLarge = 1000f, incrementSmall = 100f, incrementSlide = 1f, sigFigs = 1)]
+        private float deltaV = 5000;
 
         //A button to enable or disable the gimbal spin at the end of the guided flight
         [KSPField(isPersistant = true, guiActiveEditor = true, guiActive = false, guiName = "Gimbal Spin:", groupName = PAWAscentGroupName, groupDisplayName = PAWAscentGroupName),
@@ -70,6 +70,49 @@ namespace BenjisHardwiredLogic
         [KSPField(isPersistant = true, guiActiveEditor = false, guiActive = true, guiName = "Circuits are", groupName = PAWAscentGroupName, groupDisplayName = PAWAscentGroupName)]
         private string PAWmodInUse = "inactive";
 
+        //Shown in the Editor and in Flight
+        //A button to enable or disable if a message for this event will be shown
+        [KSPField(isPersistant = false, guiActiveEditor = true, guiActive = true, guiName = "Estimated Downrange", guiUnits = "km", guiFormat = "F1", groupName = PAWAscentGroupName, groupDisplayName = PAWAscentGroupName)]
+        private double PAWestimatedDownrange = 0;
+
         #endregion
+
+
+        /*
+         * 
+        Schleife schreiben, um die weiteste downrange zu finden.
+        verschiedene Winkel ausprobieren (25 - 45°)
+
+
+
+        double calculateRange(double dV, double angle)
+        {
+            const double g = 9.81; // Acceleration due to gravity in m/s²
+            double radians = angle * (M_PI / 180.0); // Convert angle to radians
+            double range = (pow(dV, 2) * sin(2 * radians)) / g; // Calculate range
+            return range;
+        }
+
+
+
+
+        Flight path angle (FPA) is the direct variable that determines how far you go, what apogee you reach is an effect of that.
+
+        The equation for calculating the Minimum Energy launch angle is
+        FPA = 14.325*(π - (Downrange Distance / Radius of Earth))
+
+        This results in the following values for the downrange contracts:
+        3000 km = 38.26°
+        5000 km = 33.76°
+        7500 km = 28.14°
+
+        Flight Path Angle refers to the angle of your velocity vector to the local horizontal.
+        It is not necessarily the number you plug into MechJeb, it is the angle you want your velocity vector to be at when your final stage burns out.
+        Depending on your design, your pitch angle may be slightly higher than this. You want to mess with your MechJeb ascent settings to reach this value.
+        I personally start with MJ settings of 30° and 30% and adjust from there.
+
+
+
+        */
     }
 }
